d58ea3e33f578231d8a8474b3dbcbb6992078065
Name <wangguanghan1573@gmail.com>
2023-11-22 00:48:08 +0800

    first available version

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,18 +6,30 @@
 
             <a-space>
                 <!-- 搜索框 -->
-                <a-input placeholder="请输入搜索内容" v-model="searchValue" />
+                <a-tooltip placement="topLeft" title="Not yet developed" arrow-point-at-center color="gray"
+                    :mouseEnterDelay="1.5">
+                    <a-input placeholder="请输入搜索内容" v-model="searchValue" disabled />
+                </a-tooltip>
                 <!-- 搜索按钮 -->
-                <a-button :icon="h(SearchOutlined)">Search</a-button>
+                <a-tooltip placement="topLeft" title="Not yet developed" arrow-point-at-center color="gray"
+                    :mouseEnterDelay="1.5">
+                    <a-button :icon="h(SearchOutlined)" disabled>Search</a-button>
+                </a-tooltip>
 
                 <!-- 刷新按钮 -->
-                <a-button :icon="h(ReloadOutlined)" @click="reloadTable">reload</a-button>
+                <a-tooltip placement="topLeft" title="Click to reload task list." arrow-point-at-center color="blue"
+                    :mouseEnterDelay="1.5">
+                    <a-button :icon="h(ReloadOutlined)" @click="reloadTable">reload</a-button>
+                </a-tooltip>
 
                 <a-popconfirm title="Are you sure flush?" ok-text="Yes" cancel-text="No" @confirm="flush"
                     @cancel="reloadTable">
 
                     <!-- 清除所有任务按钮 -->
-                    <a-button :icon="h(ReloadOutlined)" type="primary" danger>flush</a-button>
+                    <a-tooltip placement="topLeft" title="Click to reload flush all task!" arrow-point-at-center color="red"
+                        :mouseEnterDelay="1.5">
+                        <a-button :icon="h(ReloadOutlined)" type="primary" danger>flush</a-button>
+                    </a-tooltip>
                 </a-popconfirm>
 
 
@@ -30,37 +42,48 @@
             <a-table :columns="columns" :data-source="tasks">
 
                 <template #bodyCell="{ column, record }">
-                    <template v-if="column.key === 'errors'">
+                    <template v-if="column.dataIndex === 'errors'">
                         <template v-if="record.errors.length > 0">
-                            <a @click="goToErrorDetail(record.task_id)">
-                                {{ record.errors }}
-                            </a>
+                            <a-tooltip placement="topLeft" title="Click to view errors." arrow-point-at-center color="blue"
+                                :mouseEnterDelay="1.5">
+                                <a @click="goToErrorDetail(record.task_id)">
+                                    {{ record.errors }}
+                                </a>
+                            </a-tooltip>
                         </template>
                         <template v-else>
-                            <a-tag color="green">No errors</a-tag>
+                            <a-tag color="success">No errors</a-tag>
                         </template>
                     </template>
-                    <template v-else-if="column.key === 'logs'">
+                    <template v-else-if="column.dataIndex === 'logs'">
                         <template v-if="record.logs > 0">
-                            <a @click="goToLogDetail(record.task_id)">
-                                {{ record.logs }}
-                            </a>
+                            <a-tooltip placement="topLeft" title="Click to view logs." arrow-point-at-center color="blue"
+                                :mouseEnterDelay="1.5">
+                                <a-tag>
+                                    <a @click="goToLogDetail(record.task_id)">
+                                        {{ record.logs }}
+                                    </a>
+                                </a-tag>
+                            </a-tooltip>
                         </template>
                         <template v-else>
-                            <a-tag color="green">No logs</a-tag>
+                            <a-tag color="default">No logs</a-tag>
                         </template>
                     </template>
 
-                    <template v-else-if="column.key === 'status'">
+                    <template v-else-if="column.dataIndex === 'status'">
                         <!-- <span :style="{ backgroundColor: getStatusdColor(record.status) }">{{ record.status }}</span> -->
                         <a-tag :color="getStatusdColor(record.status)">{{ record.status }}</a-tag>
                     </template>
 
-                    <template v-else-if="column.key === 'injected'">
+
+                    <template v-else-if="column.dataIndex === 'injected'">
                         <template v-if="record.injected === true">
-                            <!-- <a @click="goToPayloadDetails(record.task_id)"><span
-                                    :style="{ backgroundColor: getInjectedColor(record.injected) }">{{ record.injected }}</span></a> -->
-                            <a-tag :color="getInjectedColor(record.injected)"><a @click="goToPayloadDetails(record.task_id)" >{{ record.injected }}</a></a-tag>
+                            <a-tooltip placement="topLeft" title="Click to view payload details." arrow-point-at-center
+                                color="blue" :mouseEnterDelay="1.5">
+                                <a-tag :color="getInjectedColor(record.injected)"><a
+                                        @click="goToPayloadDetails(record.task_id)">{{ record.injected }}</a></a-tag>
+                            </a-tooltip>
                         </template>
                         <template v-else>
                             <template v-if="record.status === TaskStatus.Terminated">
@@ -74,62 +97,84 @@
                     </template>
 
 
-                    <template v-else-if="column.key === 'action'">
+                    <template v-else-if="column.dataIndex === 'action'">
                         <a-space>
-                            <template v-if="CanStopTaskStatus.includes(record.status)">
-                                <!-- <a-button @click="stopTask(record.task_id)" type="primary"
-                                    style="background-color: #FFA500;">Stop</a-button> -->
-                                    <a-popconfirm title="Are you sure stop?" ok-text="Yes" cancel-text="No" @confirm="stopTask(record.task_id)"
-                                    @cancel="reloadTable">
-                                    <template #icon><question-circle-outlined style="color: red" /></template>
+                            <template v-if="record.status === CanStartTaskStatus">
+                                <a-tooltip placement="topLeft" title="Click to kill this task!" arrow-point-at-center
+                                    color="blue" :mouseEnterDelay="1.5">
+                                    <a-button :icon="h(PlayCircleOutlined)" type="primary" block
+                                        @click="startTask(record.task_id)">Start</a-button>
+                                </a-tooltip>
+                            </template>
+                            <template v-else>
+                                <a-tooltip placement="topLeft" title="Click to kill this task!" arrow-point-at-center
+                                    color="blue" :mouseEnterDelay="1.5">
+                                    <a-button :icon="h(PlayCircleOutlined)" type="primary" block disabled>Start</a-button>
+                                </a-tooltip>
+                            </template>
 
-                                    <!-- 清除所有任务按钮 -->
-                                    <a-button :icon="h(StopOutlined)" type="primary" danger>Stop</a-button>
+
+                            <template v-if="CanStopTaskStatus.includes(record.status)">
+                                <a-popconfirm title="Are you sure stop?" ok-text="Yes" cancel-text="No"
+                                    @confirm="stopTask(record.task_id)" @cancel="reloadTable">
+                                    <template #icon><question-circle-outlined style="color: red" /></template>
+                                    <a-tooltip placement="topLeft" title="Click to kill this task!" arrow-point-at-center
+                                        color="volcano" :mouseEnterDelay="1.5">
+                                        <a-button :icon="h(PauseCircleOutlined)" type="primary" danger>Stop</a-button>
+                                    </a-tooltip>
                                 </a-popconfirm>
                             </template>
                             <template v-else>
-                                <a-button type="primary" block disabled>Stop</a-button>
+                                <a-tooltip placement="topLeft" title="Click to kill this task!" arrow-point-at-center
+                                    color="volcano" :mouseEnterDelay="1.5">
+                                    <a-button :icon="h(PauseCircleOutlined)" type="primary" block disabled>Stop</a-button>
+                                </a-tooltip>
                             </template>
 
-
-                            <template v-if="record.status === CanStartTaskStatus">
-                                <a-button type="primary" block @click="startTask(record.task_id)">Start</a-button>
-                            </template>
-                            <template v-else>
-                                <a-button type="primary" block disabled>Start</a-button>
-                            </template>
 
                             <template v-if="CanKillTaskStatus.includes(record.status)">
-                                <!-- <a-button @click="killTask(record.task_id)" type="primary"
-                                    style="background-color: #FF3333;">Kill</a-button> -->
-                                    <a-popconfirm title="Are you sure kill?" ok-text="Yes" cancel-text="No" @confirm="killTask(record.task_id)"
-                                    @cancel="reloadTable">
-
-                                    <!-- 清除所有任务按钮 -->
-                                    <a-button :icon="h(DeleteOutlined)" type="primary" danger>Kill</a-button>
+                                <a-popconfirm title="Are you sure kill?" ok-text="Yes" cancel-text="No"
+                                    @confirm="killTask(record.task_id)">
+                                    <a-tooltip placement="topLeft" title="Click to kill this task!" arrow-point-at-center
+                                        color="pink" :mouseEnterDelay="1.5">
+                                        <a-button :icon="h(DeleteOutlined)" type="primary" danger>Kill</a-button>
+                                    </a-tooltip>
                                 </a-popconfirm>
                             </template>
                             <template v-else>
-                                <a-button type="primary" danger disabled>Kill</a-button>
+                                <a-tooltip placement="topLeft" title="Click to kill this task!" arrow-point-at-center
+                                    color="pink" :mouseEnterDelay="1.5">
+                                    <a-button :icon="h(DeleteOutlined)" type="primary" danger disabled>Kill</a-button>
+                                </a-tooltip>
                             </template>
 
-                            <!-- <a-button @click="deleteTask(record.task_id)" type="primary"
-                                style="background-color: #CC0000;" :icon="h(DeleteOutlined)">Delete</a-button> -->
 
-                                <a-popconfirm title="Are you sure delete?" ok-text="Yes" cancel-text="No" @confirm="deleteTask(record.task_id)">
-                                    <template #icon><question-circle-outlined style="color: red" /></template>
-                                    <!-- 清除所有任务按钮 -->
+                            <a-popconfirm title="Are you sure delete?" ok-text="Yes" cancel-text="No"
+                                @confirm="deleteTask(record.task_id)">
+                                <template #icon><question-circle-outlined style="color: red" /></template>
+                                <a-tooltip placement="topLeft" title="Click to delete this task!" arrow-point-at-center
+                                    color="red" :mouseEnterDelay="1.5">
                                     <a-button :icon="h(DeleteOutlined)" type="primary" danger>Delete</a-button>
-                                </a-popconfirm>
+                                </a-tooltip>
+                            </a-popconfirm>
                         </a-space>
                     </template>
 
                 </template>
-
+                <template #summary>
+                    <a-table-summary-row>
+                        <a-table-summary-cell :col-span="7" align="right">
+                            <a-typography-text>Task count: {{ tasks.length }}</a-typography-text>
+                        </a-table-summary-cell>
+                    </a-table-summary-row>
+                </template>
             </a-table>
         </a-layout-content>
         <a-layout-footer style="text-align: center">
-            Sqlmap api web ui to manager scan tasks.
+            <a-card :bordered="false">
+                <p>MySqlmap api web ui to manager scan tasks.</p>
+            </a-card>
+
         </a-layout-footer>
     </a-layout>
 
@@ -146,14 +191,60 @@ import {
     StopOutlined,
     CloseOutlined,
     DeleteOutlined,
+    PauseCircleOutlined,
+    PlayCircleOutlined,
     QuestionCircleOutlined,
 } from '@ant-design/icons-vue';
 import { notification } from 'ant-design-vue';
 
-
+const name = ref('Home');
 const router = useRouter();
 
 const [api, contextHolder] = notification.useNotification();
+
+const columns = ref([
+    {
+        title: '#',
+        dataIndex: 'index'
+    },
+    {
+        title: 'task id',
+        dataIndex: 'task_id'
+    },
+    {
+        title: 'errors',
+        dataIndex: 'errors'
+    },
+    {
+        title: 'logs',
+        dataIndex: 'logs'
+    },
+    {
+        title: 'status',
+        dataIndex: 'status'
+    },
+    {
+        title: 'injected',
+        dataIndex: 'injected',
+        filters: [
+            {
+                text: 'Injected',
+                value: true,
+            },
+            {
+                text: 'No injected',
+                value: false,
+            }
+        ],
+        onFilter: (value, record) => record.injected === value,
+    },
+    {
+        title: 'Action',
+        dataIndex: 'action'
+    },
+]);
+
+const tasks = ref([]);
 
 const openNotification = (title, desc) => {
     notification.open({
@@ -161,8 +252,6 @@ const openNotification = (title, desc) => {
         description: desc
     });
 };
-
-const name = ref('Home');
 
 var TaskStatus = {
     New: 'New',
@@ -303,46 +392,6 @@ function getInjectedColor(value) {
         return 'green';
     }
 }
-
-const columns = ref([
-    {
-        title: '#',
-        dataIndex: 'index',
-        key: 'index'
-    },
-    {
-        title: 'task id',
-        dataIndex: 'task_id',
-        key: 'task_id'
-    },
-    {
-        title: 'errors',
-        dataIndex: 'errors',
-        key: 'errors'
-    },
-    {
-        title: 'logs',
-        dataIndex: 'logs',
-        key: 'logs'
-    },
-    {
-        title: 'status',
-        dataIndex: 'status',
-        key: 'status'
-    },
-    {
-        title: 'injected',
-        dataIndex: 'injected',
-        key: 'injected'
-    },
-    {
-        title: 'Action',
-        dataIndex: 'action',
-        key: 'action'
-    },
-])
-
-const tasks = ref([]);
 
 onBeforeMount(async () => {
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'D:/work_places/devs/python-devs/mySqlmap/lib/utils/api/dist' // 指定编译目标路径为dist目录
+  },
 })
